be88b6791f19eadd0c292dc6e159b8c95f08c539
Artem Tregubov <atregubov@oneincsystems.com>
2018-03-06 14:28:08 +0700

    fix gulp file and add bower and package.json

--- TemaTre.Site.Resume.Frontend/gulpfile.js
+++ TemaTre.Site.Resume.Frontend/gulpfile.js
@@ -1,14 +1,119 @@
-var build = require('gulp-build');
- 
-var options = {
-  helpers: [{
-    name: 'addition',
-    fn: function(a, b) { return a + b; }
-  }]
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    build: { 
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    sources: { 
+        html: 'sources/*.html', 
+        js: 'sources/js/main.js',
+        style: 'sources/style/main.scss',
+        img: 'sources/img/**/*.*', 
+        fonts: 'sources/fonts/**/*.*'
+    },
+    watch: { 
+        html: 'sources/**/*.html',
+        js: 'sources/js/**/*.js',
+        style: 'sources/style/**/*.scss',
+        img: 'sources/img/**/*.*',
+        fonts: 'sources/fonts/**/*.*'
+    },
+    clean: './build'
 };
- 
-gulp.task('build', function() {
-  gulp.src('pages/*.html')
-      .pipe(build({ title: 'Some page' }, options))
-      .pipe(gulp.dest('dist'))
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Devil"
+};
+
+gulp.task('html:build', function () {
+    gulp.src(path.sources.html) 
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream: true}));
 });
+
+
+gulp.task('js:build', function () {
+    gulp.src(path.sources.js) 
+        .pipe(rigger()) 
+        .pipe(sourcemaps.init()) 
+        .pipe(uglify()) 
+        .pipe(sourcemaps.write()) 
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('style:build', function () {
+    gulp.src(path.sources.style) 
+        .pipe(sourcemaps.init()) 
+        .pipe(sass()) 
+        .pipe(prefixer()) 
+        .pipe(cssmin()) 
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css)) 
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.sources.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+
+
+gulp.task('build', [
+    'html:build',
+    'js:build',
+    'style:build',
+    'fonts:build'
+]);
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+

72ea13b62ae3e7b2e0f405dcc7c575ec0f70eb1f
Artem Tregubov <atregubov@oneincsystems.com>
2018-03-07 10:49:11 +0700

    add integration of .net core project and html+js

--- TemaTre.Site.Resume.Backend/Program.cs
+++ TemaTre.Site.Resume.Backend/Program.cs
@@ -20,6 +20,7 @@ namespace TemaTre.Site.Resume
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseWebRoot("wwwroot")
                 .Build();
     }
 }
--- TemaTre.Site.Resume.Backend/Startup.cs
+++ TemaTre.Site.Resume.Backend/Startup.cs
@@ -24,11 +24,8 @@ namespace TemaTre.Site.Resume
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            
+            app.UseStaticFiles();
         }
     }
 }

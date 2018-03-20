725e659c904583f33218dce3039008520aa9f73b
TemaTre <artem.tregubov@mail.ru>
2018-03-20 07:32:13 +0700

    Add server side handler for Feedback

---
+++ TemaTre.Site.Resume/Api/Feedback/Dto/FeedbackRequestDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemaTre.Site.Resume.Api.Feedback.Dto
+{
+    public class FeedbackRequestDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
---
+++ TemaTre.Site.Resume/Api/Feedback/FeedbackController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TemaTre.Site.Resume.Api.Feedback.Dto;
+
+namespace TemaTre.Site.Resume.Api
+{
+    [Produces("application/json")]
+    [Route("api/Feedback")]
+    public class FeedbackController : Controller
+    {
+
+        [HttpPost]
+        [ActionName("SaveFeedback")]
+        public HttpResponseMessage SaveFeedbackPost(FeedbackRequestDto request)
+        {
+            if (ModelState.IsValid && request != null)
+            {
+                // Create a 201 response.
+                var response = new HttpResponseMessage(HttpStatusCode.Accepted);
+
+                return response;
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
